14a2632bf424058eaa0e361815a911f7406907e1
Giousa <65489469@qq.com>
2021-10-28 23:02:50 +0800

    简单工厂模式

---
+++ src/com/giousa/工厂模式/Client.java
@@ -0,0 +1,65 @@
+package com.giousa.工厂模式;
+
+public class Client {
+
+    public static void main(String[] args) {
+        IProduct product = ProductFactory.getProduct(1);
+        product.doSomething();
+
+        IProduct product2 = ProductFactory.getProduct(2);
+        product2.doSomething();
+    }
+
+    public interface IProduct {
+        void doSomething();
+    }
+
+    public static class ProductA implements IProduct {
+
+        @Override
+        public void doSomething() {
+            System.out.println("I am product A");
+        }
+    }
+
+    public static class ProductB implements IProduct {
+
+        @Override
+        public void doSomething() {
+            System.out.println("I am product B");
+        }
+    }
+
+    public static class ProductC implements IProduct {
+
+        @Override
+        public void doSomething() {
+            System.out.println("I am product C");
+        }
+    }
+
+    public static class Constant {
+        public static final int PRODUCT_A = 1;
+        public static final int PRODUCT_B = 2;
+        public static final int PRODUCT_C = 3;
+    }
+
+    public static class ProductFactory {
+
+        public static IProduct getProduct(int index) {
+
+            switch (index) {
+                case Constant.PRODUCT_A:
+                    return new ProductA();
+
+                case Constant.PRODUCT_B:
+                    return new ProductB();
+
+                case Constant.PRODUCT_C:
+                    return new ProductA();
+            }
+
+            return null;
+        }
+    }
+}
