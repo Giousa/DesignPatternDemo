1affee2fcef384ed8458f4f661f8c633918b1840
Giousa <65489469@qq.com>
2022-11-29 13:23:01 +0800

    责任链模式注解

--- src/com/giousa/责任链模式/Client.java
+++ src/com/giousa/责任链模式/Client.java
@@ -2,6 +2,12 @@ package com.giousa.责任链模式;
 
 import java.util.Objects;
 
+/**
+ * 责任链模式，也叫做职责链，是将请求的发送和接收解耦，让多个接收对象都有机会处理这个请求。
+ * 当有请求发生时，可将请求沿着这条链传递，直到有对象处理它为止。
+ *
+ * 职责链模式最常见的应用就是用来开发各种框架的过滤器和拦截器，比如Spring Interceptor和Servlet Filter
+ */
 public class Client {
 
     public static void main(String[] args) {
@@ -10,6 +16,9 @@ public class Client {
 
         concreteHandlerA.setNextHandler(concreteHandlerB);
 
+        concreteHandlerA.handleRequest("requestA");
+        concreteHandlerA.handleRequest("requestB");
+        concreteHandlerA.handleRequest("requestA");
         concreteHandlerA.handleRequest("requestA");
         concreteHandlerA.handleRequest("requestB");
     }
